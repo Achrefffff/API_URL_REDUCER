ced70a04cb66570b283632eceff6fb0ee9c9fdc1
dorra chaffai <achref.chouikh@gmail.com>
2024-04-20 00:14:49 +0200

    fonction pour genener short url done

--- index.php
+++ index.php
@@ -1,21 +1,65 @@
 <?php
+header('Content-Type: application/json');
+$_POST = json_decode(file_get_contents('php://input'), true);
 
-// Récupérer le contenu du corps de la requête
-$json_input = file_get_contents('php://input');
+// Paramètres de connexion à la base de données
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "reducer";
 
-// Convertir le JSON en tableau associatif
-$data = json_decode($json_input, true);
+// Créer une connexion à la base de données
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-// Vérifier si une URL longue a été fournie
-if(isset($data['long_url'])) {
-    // Récupérer l'URL longue depuis les données
-    $long_url = $data['long_url'];
+// Vérifier la connexion
+if (!$conn) {
+    die("Connexion échouée : " . mysqli_connect_error());
+}
 
-    // Répondre avec l'URL raccourcie (temporairement renvoyée comme l'URL longue pour le moment)
-    echo json_encode(array('short_url' => $long_url));
+// Fonction pour générer un URL court aléatoire
+function generateShortURL($length = 8) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $shortURL = '';
+    for ($i = 0; $i < $length; $i++) {
+        $index = rand(0, strlen($characters) - 1);
+        $shortURL .= $characters[$index];
+    }
+    return $shortURL;
+}
+
+// Vérifier si une URL longue a été fournie dans le corps de la requête POST
+if(isset($_POST) && isset($_POST['long_url'])) {
+    // Récupérer l'URL longue depuis le corps de la requête POST
+    $long_url = $_POST['long_url'];
+
+    // Générer un URL court
+    $short_url = generateShortURL();
+
+    // Vérifier si l'URL court existe déjà dans la base de données
+    $sql = "SELECT * FROM urls WHERE short_url = '$short_url'";
+    $result = mysqli_query($conn, $sql);
+
+    // Si l'URL court existe déjà, générer un nouvel URL court
+    if(mysqli_num_rows($result) > 0) {
+        $short_url = generateShortURL();
+    }
+
+    // Insérer une nouvelle URL dans la table urls
+    $sql = "INSERT INTO urls (long_url, short_url, created_at) VALUES ('$long_url', '$short_url', NOW())";
+
+    if (mysqli_query($conn, $sql)) {
+        // Répondre avec l'URL court généré
+        echo json_encode(array('short_url' => $short_url));
+    } else {
+        echo json_encode(array('error' => 'Erreur lors de la création de l\'URL court'));
+    }
+
 } else {
     // Si aucune URL longue n'a été fournie, renvoyer un message d'erreur
     echo json_encode(array('error' => 'Aucune URL longue fournie'));
 }
+
+// Fermer la connexion à la base de données
+mysqli_close($conn);
 
 ?>
