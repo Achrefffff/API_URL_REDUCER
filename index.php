4927e1a86af3bd288a9392fc32e254ed20f78179
dorra chaffai <achref.chouikh@gmail.com>
2024-04-20 00:54:20 +0200

    en-tetes cors ajouté

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 $_POST = json_decode(file_get_contents('php://input'), true);
 
